96a644c9ee10b40b92f40a94ecaf08d447d88ea9
Vivek Pandey1 <vivek_pandey1@epam.com>
2022-12-06 16:18:14 +0530

    initial commit

--- Campus-Comrade/src/firebase.js
+++ Campus-Comrade/src/firebase.js
@@ -1,13 +1,5 @@
 import firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyCPCaHQgZM9ijwdsfp7qVnSRFno2ZlRPos",
-  authDomain: "campus-comrade.firebaseapp.com",
-  projectId: "campus-comrade",
-  storageBucket: "campus-comrade.appspot.com",
-  messagingSenderId: "1035539756571",
-  appId: "1:1035539756571:web:b9439acc84295c71142abe",
-  measurementId: "G-3WBTXXQTVQ",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
--- disney-hotstar-clone/src/Firebase.js
+++ disney-hotstar-clone/src/Firebase.js
@@ -3,15 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD_7vUBfyJF0NetgMJVJkcdq7Xk11_-HPg",
-  authDomain: "disneyplus-clone-f3f36.firebaseapp.com",
-  projectId: "disneyplus-clone-f3f36",
-  storageBucket: "disneyplus-clone-f3f36.appspot.com",
-  messagingSenderId: "250975431460",
-  appId: "1:250975431460:web:7a52dbb51b87cca6db61b1",
-  measurementId: "G-N2W9DDN1L9",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
